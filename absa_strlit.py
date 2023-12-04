e4e9ed7e5ce2e19fcffd221f1895498f23c7ef06
Aratrik Basak <75318952+Aratrik-02@users.noreply.github.com>
2023-12-04 11:12:52 +0530

    Update absa_strlit.py

--- absa_strlit.py
+++ absa_strlit.py
@@ -1,22 +1,19 @@
 import streamlit as st
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer  # Ensure you import this if you're using it in your Colab notebook
+from sklearn.feature_extraction.text import TfidfVectorizer 
 
-# Load your pre-trained model and TF-IDF vectorizer using pickle
-# Replace 'your_model_path.pkl' and 'your_tfidf_vectorizer_path.pkl' with the actual file paths of your trained model and TF-IDF vectorizer
+
 with open('trained_model.sav', 'rb') as model_file:
     model = pickle.load(model_file)
 
 with open('tfidf_vector.sav', 'rb') as vectorizer_file:
     tfidf_vectorizer = pickle.load(vectorizer_file)
 
-# Function to predict polarity
 def predict_polarity(sentence):
     new_input_tfidf = tfidf_vectorizer.transform([sentence])
     predicted_polarity = model.predict(new_input_tfidf)
     return predicted_polarity[0]
 
-# Streamlit app
 def main():
     st.title('Sentence Polarity Prediction')
     sentence = st.text_input('Enter a sentence:')
